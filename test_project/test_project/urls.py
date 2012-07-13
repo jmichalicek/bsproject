4b1338cb945e55bad4cb7e26749b3dc73f917eb0
Justin Michalicek <jmichalicek@gmail.com>
2012-07-12 21:44:05 -0400

    Correct test_project urls.py

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^projects/', include('bsblog.urls'))
+    url(r'^projects/', include('bsproject.urls'))
 )
